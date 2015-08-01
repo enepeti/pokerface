0b87ab6e63d27ea5c69fa456e5909f72b8a8608d
salierri <g.gergely314@gmail.com>
2015-08-01 14:20:48 +0200

    Design update of the main page

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ var config = {
     mongo: {
         path: "mongodb://localhost/pokerarc",
     },
-    timeout: 5000,
+    timeout: 10000,
     weights: [10,20,30,40,50],
-    questionCounts: [2, 2, 5, 5, 5]
+    questionCounts: [8, 5, 5, 5, 5]
 };
 module.exports = config;
