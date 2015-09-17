a73101bb4f7d29913ea935f34ba5faec5767775a
pke <enepeti@gmail.com>
2015-09-17 19:20:27 +0200

    script kulon fajlban

---
+++ public/pokerface.js
@@ -0,0 +1,56 @@
+var socket = io();
+var lastAnswer = null;
+var firstQuestion;
+
+socket.on('question', function(data) {
+    $('#answerButtona').css("background", "transparent");
+    $('#answerButtonb').css("background", "transparent");
+    $('#answerButtonc').css("background", "transparent");
+    $('#answera').css("background", "transparent");
+    $('#answerb').css("background", "transparent");
+    $('#answerc').css("background", "transparent");
+    
+    $('#question').html(data.question);
+    $('#answera').html(data.answers[0]);
+    $('#answerb').html(data.answers[1]);
+    $('#answerc').html(data.answers[2]);
+    lastAnswer = null;
+    
+    if(firstQuestion) {
+        firstQuestion = false;
+        $('#questionaire').css("display", "block");
+    }
+});
+
+socket.on('correct', function(correctAnswer) {
+    if(lastAnswer) {
+        $('#answerButton' + lastAnswer).css("background", "red");
+        $('#answer' + lastAnswer).css("background", "red");
+    }
+    
+    $('#answerButton' + correctAnswer).css("background", "green");
+    $('#answer' + correctAnswer).css("background", "green");
+});
+
+socket.on('score', function(score) {
+    $('correctAnswers').html('Ebben a körben a jó válaszaid száma: ' + score); 
+    $('#questionaire').css("display", "none");
+    $('#correctAnswers').css("display", "block");
+    firstQuestion = true;
+});
+
+function sendAnswer(ansLetter) {
+    if(!lastAnswer) {
+        socket.emit('answer', ansLetter);
+        $('#answerButton' + ansLetter).css("background", "yellow");
+        $('#answer' + ansLetter).css("background", "yellow");
+        lastAnswer = ansLetter;
+    }
+}
+
+function sendName() {
+    var name = $('#team').val() + '-' + $('#name').val();
+    socket.emit('username', name);
+    $('#setName').css("display", "none");
+    firstQuestion = true;
+}
